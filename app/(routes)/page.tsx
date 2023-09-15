18123108ed4a7c648a1e322b2374bb680da32167
pedroht <torres.pedrohen@gmail.com>
2023-09-14 23:28:08 -0300

    feat: create billboard component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,17 @@
-export default function HomePage() {
+import { getBillboard } from "@/actions/get-billboard";
+import { Billboard } from "@/components/billboard";
+import { Container } from "@/components/ui/container";
+
+export const revalidate = 0;
+
+export default async function HomePage() {
+  const billboard = await getBillboard("695bf495-11dc-4df0-b6cd-35e99b8615e1");
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   );
 }
