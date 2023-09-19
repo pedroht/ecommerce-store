0ff2c4dc8b7c93008deb704beced291befcaca22
pedroht <torres.pedrohen@gmail.com>
2023-09-18 23:47:36 -0300

    feat: add product info

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import { getProduct } from "@/actions/get-product";
 import { getProducts } from "@/actions/get-products";
 import { Gallery } from "@/components/gallery";
+import { Info } from "@/components/info";
 import { ProductList } from "@/components/products-list";
 import { Container } from "@/components/ui/container";
 
@@ -25,8 +26,7 @@ export default async function ProductPage({ params }: ProductPageProps) {
             <Gallery images={product.images} />
 
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-              {/* Info */}
-              Info
+              <Info data={product} />
             </div>
           </div>
 
