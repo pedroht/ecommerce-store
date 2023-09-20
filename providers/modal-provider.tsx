9849962747fbbeb7a0c00aefc07a501d8a505592
pedroht <torres.pedrohen@gmail.com>
2023-09-20 19:23:47 -0300

    feat: add modal provider

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { PreviewModal } from "@/components/preview-modal";
+
+import { useMounted } from "@/hooks/use-mounted";
+
+export function ModalProvider() {
+  const isMounted = useMounted();
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <PreviewModal />
+    </>
+  );
+}
