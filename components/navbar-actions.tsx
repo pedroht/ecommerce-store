d9b4425bbd7c5d5e7c5985b3c33268e4eabb2f2c
pedroht <torres.pedrohen@gmail.com>
2023-09-20 20:16:26 -0300

    feat: add cart items component

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
 import { useCart } from "@/hooks/use-cart";
 import { useMounted } from "@/hooks/use-mounted";
-import { ShoppingBag } from "lucide-react";
 
 export function NavbarActions() {
+  const router = useRouter();
   const isMounted = useMounted();
   const cart = useCart();
 
@@ -15,7 +18,10 @@ export function NavbarActions() {
 
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center px-4 py-2">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center px-4 py-2"
+      >
         <ShoppingBag size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white">
           {cart.items.length}
