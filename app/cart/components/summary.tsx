4d0b7c0cd28ab89645ce20f5519210dfa148e2bb
pedroht <torres.pedrohen@gmail.com>
2023-09-20 21:19:24 -0300

    fix: spelling

--- app/cart/components/summary.tsx
+++ app/cart/components/summary.tsx
@@ -14,7 +14,7 @@ export function Summary() {
   const removeAll = useCart((state) => state.removeAll);
 
   useEffect(() => {
-    if (searchParams.get("sucess")) {
+    if (searchParams.get("success")) {
       toast.success("Payment completed");
       removeAll();
     }
